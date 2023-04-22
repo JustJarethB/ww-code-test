a3060575bbc36aeff1fa10fd85ce151511f19a18
Jareth <31932204+JustJarethB@users.noreply.github.com>
2023-04-22 13:23:58 +0100

    build: add typescript

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (<div>
+  <h1 className="text-slate-100">Hello</h1>
+</div>
+
+);
+
+
+export default App;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configure-store';
 import App from './App';
+import { Provider } from 'react-redux';
 
 const history = createBrowserHistory();
 const store = configureStore(history);
 const rootEl = document.getElementById('app');
 
 ReactDOM.render(
-  <App history={history} store={store} />,
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
   rootEl,
 );
